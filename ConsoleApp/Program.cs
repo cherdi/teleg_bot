18e77ed3115c45c9e5d466464129498c1245e501
cherdi <maksimmarozau@gmail.com>
2017-05-24 13:56:53 +0300

    Update Program.cs
    Commit 3

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -12,6 +12,7 @@ namespace ConsoleApp
         {
             Console.WriteLine("Hello world!");
             Console.WriteLine("Hello world2!");
+            Console.WriteLine("Hello world3!");
             Console.ReadKey();
         }
     }
